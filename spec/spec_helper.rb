fa87e21106877537d2563d288191e223b52c8125
Marcelo Manzan <manzan@gmail.com>
2010-11-29 00:39:25 -0200

    spec_helper.rb now working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+
 require 'rubygems'
+require 'lastversion'
 require 'rspec'
 
 Dir["#{File.expand_path('../support', __FILE__)}/*.rb"].each do |file|
