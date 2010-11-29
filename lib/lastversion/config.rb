2fb8a3281fb6777405aadcd699adb852b615a3e4
Marcelo Manzan <manzan@gmail.com>
2010-11-28 23:08:21 -0200

    loading configuration yaml

--- lib/lastversion/config.rb
+++ lib/lastversion/config.rb
@@ -1,3 +1,14 @@
+require 'yaml'
 module LastVersion
   CONFIG = {}
+
+  def self.load_config path
+    return CONFIG unless File.exists? path
+    CONFIG.merge! YAML.load_file(path)
+  rescue TypeError => e
+    puts "could not load #{path}: #{e.inspect}"
+  end
+
+  load_config File.expand_path('../config/lastversion.yml', __FILE__)
+  load_config '.versionrc' # from working folder
 end
