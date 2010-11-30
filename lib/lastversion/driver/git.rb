6948d1bcbec1157c150a08c61a8841d4afe5b139
Marcelo Manzan <manzan@gmail.com>
2010-11-30 01:15:01 -0200

    getting all version tags from git repository

--- lib/lastversion/driver/git.rb
+++ lib/lastversion/driver/git.rb
@@ -1,9 +1,22 @@
 module LastVersion
   module Driver
     class Git
+      attr_reader :mask
+      def initialize
+        @mask = Parser::VersionMask.new(CONFIG["versioning"]["patterns"]["version"])
+      end
+
       def history_log commit_base, top = nil
         top = "-n#{ top }" unless top.nil?
         `git log --pretty=oneline --no-color --no-notes #{ top } #{ commit_base }`.gsub(/^(\w+)\s.*$/, '\1').split("\n")
+      end
+
+      def all_tags
+        `git tag -l`.split("\n")
+      end
+
+      def all_version_tags
+        @version_tags ||= all_tags.map{ |tag| mask.parse(tag) }.compact.sort.map{ |tag| mask.format(tag) }.reverse
       end
     end
   end
