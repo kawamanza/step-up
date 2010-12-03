8cd3679c2b5cedd68f51f727ab3893dd7bb332db
Eric Fer <eric.fer@gmail.com>
2010-12-03 10:11:27 -0200

    put it back tests related to notes

--- spec/lib/lastversion/driver/git_spec.rb
+++ spec/lib/lastversion/driver/git_spec.rb
@@ -38,4 +38,27 @@ describe LastVersion::Driver::Git do
       @driver.class.last_version("cdd4d5a").should be == "v0.0.0+"
     end
   end
+  
+
+  context "fetching notes" do
+    context "from test_* sections" do
+      before do
+        @driver.stubs(:notes_sections).returns(%w[test_changes test_bugfixes test_features])
+      end
+      it "should get all objects with notes" do
+        @driver.all_objects_with_notes("f4cfcc2").should be == {"test_changes" => ["8299243c7dac8f27c3572424a348a7f83ef0ce28", "2fb8a3281fb6777405aadcd699adb852b615a3e4"], "test_bugfixes" => [], "test_features" => []}
+      end
+      it "should get all notes messages" do
+        objects_with_notes = @driver.all_objects_with_notes("f4cfcc2")
+        @driver.notes_messages(objects_with_notes).should be == {"test_changes" => ["removing files from gemspec\n  .gitignore\n  lastversion.gemspec\n", "loading default configuration yaml\n\nloading external configuration yaml\n"], "test_bugfixes" => [], "test_features" => []}
+      end
+    end
+  end
+
+
+  context "checking helper methods" do
+    it "should load default notes' sections" do
+      @driver.send(:notes_sections).should be == LastVersion::CONFIG["notes"]["sections"]
+    end
+  end  
 end
