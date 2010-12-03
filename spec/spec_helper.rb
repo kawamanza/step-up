a64ec4a4b47001fa3b876e3870f79f1d1a2e8f59
Marcelo Manzan <manzan@gmail.com>
2010-12-03 10:26:16 -0200

    mocking with 'mocha'... rightly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,11 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'lastversion'
 require 'rspec'
-require 'mocha'
 
 Dir["#{File.expand_path('../support', __FILE__)}/*.rb"].each do |file|
   require file
 end
 
+RSpec.configure do |config|
+  config.mock_with :mocha
+end
