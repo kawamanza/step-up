5f59e9f7db96fe28362e397fb0d8fa4f3706a348
Marcelo Manzan <manzan@gmail.com>
2010-12-03 13:24:41 -0200

    more tests for VersionMask formatting

--- spec/lib/lastversion/parser/version_mask_spec.rb
+++ spec/lib/lastversion/parser/version_mask_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe LastVersion::Parser::VersionMask do
+  before do
+    lambda {
+      @mask = LastVersion::Parser::VersionMask.new("v0.0.0.9.9.rc9")
+    }.should_not raise_error ArgumentError
+  end
+
+
   context "parsing" do
-    before do
-      lambda {
-        @mask = LastVersion::Parser::VersionMask.new("v0.0.0.9.9.rc9")
-      }.should_not raise_error ArgumentError
-    end
     it "should parse" do
       @mask.parse("v0.1.0.rc3").should be == [0, 1, 0, 0, 0, 3]
       @mask.parse("v0.1.0").should be == [0, 1, 0, 0, 0, 0]
@@ -16,10 +18,18 @@ describe LastVersion::Parser::VersionMask do
       @mask.parse("v0.1.rc3").should be_nil
       @mask.parse("note-v0.1.0-1").should be_nil
     end
+  end
+
+
+  context "formatting" do
     it "should format" do
       @mask.format([0, 1, 0, 0, 0, 3]).should be == "v0.1.0.rc3"
       @mask.format([0, 1, 0, 0, 0, 0]).should be == "v0.1.0"
       @mask.format([0, 1, 4, 5, 0, 3]).should be == "v0.1.4.5.rc3"
+    end
+    it "should not format" do
+      lambda {@mask.format([0, 1, 0, 0, 3])}.should raise_error ArgumentError
+      lambda {@mask.format([0, 1, 0, 0, 0, 0, 0])}.should raise_error ArgumentError
     end
   end
 end
