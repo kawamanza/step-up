c100518e7a3d59bf13afa9418c87254c29fadd3d
Marcelo Manzan <manzan@gmail.com>
2010-12-15 02:56:48 -0200

    command line 'notes add'

--- lib/step-up/cli.rb
+++ lib/step-up/cli.rb
@@ -3,6 +3,7 @@ require 'step-up'
 
 module StepUp
   class CLI < Thor
+    include Thor::Actions
     map %w(--version -v) => :gem_version
     
     default_task :version
@@ -34,15 +35,78 @@ module StepUp
       end
     end
 
-    desc "notes [object]", "show notes for the next version"
-    method_options :clean => :boolean
-    def notes(commit_base = nil)
-      puts StepUp::Driver::Git.unversioned_notes(commit_base, options[:clean])
+    desc "notes ACTION [object] [OPTIONS]", "show notes for the next version"
+    method_options :clean => :boolean, :steps => :boolean, :"-m" => :string
+    def notes(action = "show", commit_base = nil)
+      unless %w[show add help].include?(action)
+        commit_base ||= action
+        action = "show"
+      end
+      if self.respond_to?("notes_#{action}")
+        send("notes_#{action}", commit_base)
+      else
+        puts "invalid action: #{action}"
+      end
     end
     
     desc "-v, --version", "show the last version of the gem"
     def gem_version
       puts StepUp::VERSION
+    end
+
+    protected
+
+    def notes_show(commit_base = nil)
+      puts StepUp::Driver::Git.unversioned_notes(commit_base, options[:clean])
+    end
+
+    def notes_add(commit_base = nil)
+      message = options[:m] 
+      message = nil if options[:m] =~ /^(|m)$/
+      message ||= get_message("Note message:\n>>", " >")
+      unless message.empty?
+        driver = StepUp::Driver::Git.new
+        section = choose(CONFIG["notes"]["sections"], "Choose a section to add the note:")
+        return if section.nil? || ! CONFIG["notes"]["sections"].include?(section)
+        steps = driver.steps_for_add_notes(section, message, commit_base)
+        if options[:steps]
+          puts steps.join("\n")
+        else
+          steps.each do |step|
+            run step
+          end
+        end
+      end
+    end
+
+    private
+
+    def choose(list, enunciation)
+      puts enunciation
+      list.each_with_index do |item, index|
+        puts "  #{ index + 1 }. #{ item }"
+      end
+      value = ask(">>")
+      if value =~ /^\d+$/
+        value = value.to_i
+        value > 0 && list.size > value ? list[value - 1] : nil
+      else
+        value.empty? ? nil : value
+      end
+    end
+
+    def get_message(prompt1, prompt2, total_breaks = 1)
+      lines = []
+      prompt = prompt1
+      empty = 0
+      line = nil
+      begin
+        lines << line unless line.nil?
+        line = ask(prompt)
+        prompt = prompt2
+        empty = line.empty? ? empty + 1 : 0
+      end while empty < total_breaks
+      lines.join("\n").gsub(/\n+\z/, '').gsub(/\A\n+/, '')
     end
   end
 end
