9fd9fed2b7ab48c3a904f6f0b3377b073c540099
Marcelo Manzan <manzan@gmail.com>
2010-12-17 17:55:38 -0200

    refactoring on version.rb

--- lib/step-up/version.rb
+++ lib/step-up/version.rb
@@ -1,28 +1,7 @@
 module StepUp
-  path = File.expand_path('../..', __FILE__)
-  v = nil
-  if path =~ /\/step-up-([\w\.\-]+)/
-    v = $1
-  end
-  if v.nil?
-    $:.each do |path|
-      if path =~ /\/step-up-([\w\.\-]+)/
-        v = $1
-        break
-      end
-    end
-  end
-  if v.nil?
-    path = File.expand_path('../../../.git', __FILE__)
-    if File.exists?(path)
-      v = Driver::Git.last_version
-    end
-  end
-  if v.nil?
-    VERSION = "0.0.0"
-  else
-    v.sub!(/^v/, '')
-    v.sub!(/\+\d*$/, '')
-    VERSION = v
-  end
+  version = nil
+  version = $1 if ::File.expand_path('../..', __FILE__) =~ /\/step-up-([\w\.\-]+)/
+  version = Driver::Git.last_version if version.nil? && ::File.exists?(::File.expand_path('../../../.git', __FILE__))
+  version = "0.0.0" if version.nil?
+  VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
 end
