46efa3d2be3c6919a75287cc225837fd7300d01c
Eric Fer <eric.fer@gmail.com>
2010-12-27 20:28:09 -0200

    Removed hardcoded zero version once last_version_tag will provide it

--- lib/step-up/version.rb
+++ lib/step-up/version.rb
@@ -1,7 +1,6 @@
 module StepUp
   version = nil
   version = $1 if ::File.expand_path('../..', __FILE__) =~ /\/step-up-([\w\.\-]+)/
-  version = "0.0.0" if version.nil?
   version = Driver::Git.new.last_version_tag if version.nil? && ::File.exists?(::File.expand_path('../../../.git', __FILE__))
   VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
 end
