522edc9e0284af1c78f9c3f9e70905796f44aad0
Marcelo Manzan <manzan@gmail.com>
2011-03-18 11:40:47 -0300

    tiny change on version.rb to ensure that the regular expression have no conflict with other paths of gems

--- lib/step-up/version.rb
+++ lib/step-up/version.rb
@@ -1,6 +1,6 @@
 module StepUp
   version = nil
-  version = $1 if ::File.expand_path('../..', __FILE__) =~ /\/step-up-([\w\.\-]+)/
+  version = $1 if ::File.expand_path('../..', __FILE__) =~ /\/step-up-(\d[\w\.]+)/
   version = Driver::Git.new.last_version_tag if version.nil? && ::File.exists?(::File.expand_path('../../../.git', __FILE__))
   VERSION = version.gsub(/^v?([^\+]+)\+?\d*$/, '\1')
 end
