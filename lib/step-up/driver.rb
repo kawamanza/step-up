dc4258eec91361a387cd468a73551710dabca93f
Marcelo Manzan <manzan@gmail.com>
2011-03-18 23:50:47 -0300

    Driver::Base: cache results of methods if they are invoked with the same parameters

--- lib/step-up/driver.rb
+++ lib/step-up/driver.rb
@@ -3,8 +3,28 @@ module StepUp
     autoload :Git, 'step-up/driver/git.rb'
     class Base
       attr_reader :mask
+      attr_reader :cache
       def initialize(mask = nil)
         @mask = Parser::VersionMask.new(mask || CONFIG.versioning.version_mask)
+        @cache = {}
+      end
+
+      def method_missing(called_method, *args, &block)
+        if called_method.to_s =~ /^cached_(.+)$/
+          method = $1
+          if respond_to?(method)
+            if block_given?
+              send(method, *args, &block)
+            else
+              cache[method] ||= {}
+              cache[method][args] ||= send(method, *args)
+            end
+          else
+            super
+          end
+        else
+          super
+        end
       end
     end
   end
