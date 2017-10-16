03f50a67fa3ad121772aab1372804cb5987e9d99
Marcelo Manzan <manzan@gmail.com>
2017-10-16 12:14:41 -0200

    notes_util.rb :: forcing encoding before parse message

--- lib/step-up/notes_util.rb
+++ lib/step-up/notes_util.rb
@@ -1,7 +1,7 @@
 module StepUp
   module NotesUtil
     def parse_message(message)
-      message = message.rstrip.gsub(/^((?:  )*)( )?([^ \-\n])/){ "%s  %s %s" % [$1, $2 || '-', $3] }
+      message = message.rstrip.force_encoding('utf-8').gsub(/^((?:  )*)( )?([^ \-\n])/){ "%s  %s %s" % [$1, $2 || '-', $3] }
       begin
         changed = message.sub!(/^(\s*-\s.*?\n)(?:\s*\n)+(\s*-\s)/, '\1\2')
       end until changed.nil?
